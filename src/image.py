0df012dd4f472e0e9a7a11a2aff8db91585a1366
Tilman Seeßelberg <tilman.seesselberg@hm.edu>
2023-11-02 22:25:23 +0100

    window manager try 1

--- src/image.py
+++ src/image.py
@@ -1,38 +1,54 @@
 from PIL import Image, ImageDraw
 import logging
-logging.basicConfig(level=logging.DEBUG)
+import datetime
 
+logging.basicConfig(level=logging.DEBUG)
 
 DEBUG = True
 # Define image dimensions
 width = 480
 height = 800
 
+class WindowManager:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.img = None
+        self.draw = None
 
+    def create_window(self):
+        # Create new image object
+        self.img = Image.new('RGB', (self.width, self.height), color='white')
+        # Draw grid
+        self.draw = ImageDraw.Draw(self.img)
+        self.draw.line((self.width/2, 0, self.width/2, self.width/2), fill='black', width=2)
+        self.draw.line((0, self.width/2, self.width, self.width/2), fill='black', width=2)
 
-def get_empty_frame():
-    # Create new image object
-    img = Image.new('RGB', (width, height), color='white')
-    # Draw grid
-    draw = ImageDraw.Draw(img)
-    # Draw horizontal line
-    draw.line((width/2, 0, width/2, width/2), fill='black', width=2)
-    # Draw vertical line
-    draw.line((0, width/2, width, width/2), fill='black', width=2)
-    return img
+    def add_to_window(self, func):
+        func(self.img, self.draw)
 
+def add_time(img, draw):
+    # Get current time
+    now = datetime.datetime.now()
+    time_string = now.strftime("%H:%M:%S")
+    
+    # Write time to top right corner
+    draw.text((width - 10, 10), time_string, fill='black')
 
 def main():
-    img = get_empty_frame()
+    wm = WindowManager(width, height)
+    wm.create_window()
+    wm.add_to_window(add_time)
+    
     # Display image
     if not DEBUG :
         import epd7in5_V2
         epd = epd7in5_V2.EPD()
         epd.init()
-        epd.display(epd.getbuffer(img))
+        epd.display(epd.getbuffer(wm.img))
         epd.sleep()
     else:
-        img.save('output_image.png')
+        wm.img.save('output_image.png')
         print('Image saved to output_image.png')
 
 if __name__ == "__main__":
