e76a766cce2439de5bccf792ca72b650efb212c6
Tilman Seeßelberg <tilman.seesselberg@hm.edu>
2023-11-02 22:21:29 +0100

    modularization

--- src/image.py
+++ src/image.py
@@ -1,28 +1,39 @@
 from PIL import Image, ImageDraw
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
 DEBUG = True
 # Define image dimensions
 width = 480
 height = 800
 
-# Create new image object
-img = Image.new('RGB', (width, height), color='white')
 
-# Draw grid
-draw = ImageDraw.Draw(img)
-# Draw horizontal line
-draw.line((width/2, 0, width/2, width/2), fill='black', width=2)
-# Draw vertical line
-draw.line((0, width/2, width, width/2), fill='black', width=2)
 
-# save image
+def get_empty_frame():
+    # Create new image object
+    img = Image.new('RGB', (width, height), color='white')
+    # Draw grid
+    draw = ImageDraw.Draw(img)
+    # Draw horizontal line
+    draw.line((width/2, 0, width/2, width/2), fill='black', width=2)
+    # Draw vertical line
+    draw.line((0, width/2, width, width/2), fill='black', width=2)
+    return img
 
-if not DEBUG :
+
+def main():
+    img = get_empty_frame()
     # Display image
-    import epd7in5_V2
-    epd = epd7in5_V2.EPD()
-    epd.init()
-    epd.display(epd.getbuffer(img))
-    epd.sleep()
-else:
-    img.save('output_image.png')
-    print('Image saved to output_image.png')
+    if not DEBUG :
+        import epd7in5_V2
+        epd = epd7in5_V2.EPD()
+        epd.init()
+        epd.display(epd.getbuffer(img))
+        epd.sleep()
+    else:
+        img.save('output_image.png')
+        print('Image saved to output_image.png')
+
+if __name__ == "__main__":
+    main()
