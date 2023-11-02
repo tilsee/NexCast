117f0659fea1065258f3ef940763225576f27397
Tilman Seeßelberg <tilman.seesselberg@hm.edu>
2023-11-02 21:05:23 +0100

    testdrölf

---
+++ src/test2.py
@@ -0,0 +1,50 @@
+# import required python libraries
+import os
+
+# get display from Waveshare library
+import epd7in5_V2
+
+# get functions from Pillow
+from PIL import Image, ImageDraw, ImageFont
+
+# image directory
+pic_dir = 'pic'
+
+try:
+    # initialize display
+    epd_disp = epd7in5_V2.EPD()
+    epd_disp.init()
+    
+    # clear display, 0 is black, 255 is white
+    epd_disp.Clear(255)
+    
+    # reverse width and height as display is sideways
+    w = epd_disp.height
+    h = epd_disp.width
+    print('width', w)
+    print('height', h)
+    
+    #define fonts
+    top_font = ImageFont.truetype(os.path.join(pic_dir, 'Avenir Next.ttc'), 18, index=1)
+    bottom_font = ImageFont.truetype(os.path.join(pic_dir, 'Avenir Next.ttc'), 16, index=5)
+    
+    # define and draw background
+    image = Image.new(mode='1', size=(w, h),color=255)
+    #draw = ImageDraw.Draw(image)
+    
+    # position and draw text
+    #draw.text((15, 0), 'Welcome to the Workshop!', font=top_font, fill=0, align='left')
+    #draw.text((10, 150), 'https://dronebotworkshop.com', font=bottom_font, fill=0, align='left')
+    
+    # get robot image 
+    dbwsbot = Image.open('dbws-robot.bmp')
+    
+    # paste image onto background image
+    image.paste(dbwsbot, (80, 35))
+    
+    # write buffer contents to display
+    epd_disp.display(epd_disp.getbuffer(image))
+    epd_disp.sleep()
+    
+except IOError as e:
+    print(e)
