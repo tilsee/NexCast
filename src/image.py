15e70d36afa5baec6a3f4e79b014297705bf9caa
Tilman Seeßelberg <tilman.seesselberg@hm.edu>
2023-11-02 23:46:01 +0100

    some stuff works

--- src/image.py
+++ src/image.py
@@ -1,67 +1,83 @@
 from PIL import Image, ImageDraw, ImageFont
-import logging
 import datetime
+small_font_name = "Freedom-10eM.ttf"
+small_font = ImageFont.truetype(small_font_name, 14)
+medium_font = ImageFont.truetype(small_font_name, 14)
+large_font = ImageFont.truetype(small_font_name, 14)
 
-logging.basicConfig(level=logging.DEBUG)
+class FrameWithGrid:
+    def __init__(self):
+        self.h_black_image = Image.new('1', (480, 800), 255)
+        self.draw_black = ImageDraw.Draw(self.h_black_image)
+        self.draw_grid_lines()
 
-DEBUG = True
-# Define image dimensions
-width = 480
-height = 800
+    def draw_grid_lines(self):
+        # Draw horizontal grid lines
+        line_y = 240
+        while line_y < 800:
+            self.draw_black.line([(0, line_y), (480, line_y)], fill=0)
+            line_y += 80
 
-class WindowManager:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.imgs = [None, None, None]
-        self.draws = [None, None, None]
+        # Draw the line splitting the top section into two squares
+        self.draw_black.line([(0, 240), (480, 240)], fill=0)
 
-    def create_window(self):
-        for i in range(3):
-            # Create new image object
-            self.imgs[i] = Image.new('RGB', (self.width, self.height), color='white')
-            # Draw grid
-            self.draws[i] = ImageDraw.Draw(self.imgs[i])
-            self.draws[i].line((self.width/2, 0, self.width/2, self.width/2), fill='black', width=2)
-            self.draws[i].line((0, self.width/2, self.width, self.width/2), fill='black', width=2)
+    def allocate_window(self, img, window_number=None):
+        if window_number is None:
+            window_number = 1
 
-    def add_to_window(self, input_img, window_number):
-        # Scale image to fit window
-        input_img.thumbnail((self.width, self.height))
-        # Add image to window
-        self.imgs[window_number].paste(input_img, (0, 0))
+        if window_number == 1:
+            img = img.resize((240, 240))
+            self.h_black_image.paste(img, (0, 0))
+        elif window_number == 2:
+            img = img.resize((240, 240))
+            self.h_black_image.paste(img, (240, 0))
+        elif window_number == 3:
+            img = img.resize((480, 560))
+            self.h_black_image.paste(img, (0, 240))
+        else:
+            raise ValueError("Invalid window number. Choose from 1, 2, or 3.")
 
-def add_time():
-    # Get current time
-    now = datetime.datetime.now()
-    time_string = now.strftime("%H:%M:%S")
-    
-    # Create new image object
-    img = Image.new('RGB', (width, height), color='white')
-    draw = ImageDraw.Draw(img)
-    
-    # Write time to top right corner
-    draw.text((0, 10), time_string, fill='black')
-    
-    return img
+    def save_frame(self, filename):
+       self.h_black_image.save(filename)
+       print("Frame saved to", filename)
 
-def main():
-    wm = WindowManager(width, height)
-    wm.create_window()
-    wm.add_to_window(add_time(), 0)
-    
-    # Display image
-    if not DEBUG :
-        import epd7in5_V2
-        epd = epd7in5_V2.EPD()
-        epd.init()
-        for img in wm.imgs:
-            epd.display(epd.getbuffer(img))
-        epd.sleep()
-    else:
-        for i, img in enumerate(wm.imgs):
-            img.save(f'output_image_{i}.png')
-        print('Images saved to output_image_0.png, output_image_1.png, output_image_2.png')
+def get_date():
+	now = datetime.datetime.now()
+	return {
+		'weekday': now.isoweekday(),
+		'day': str(now.day),
+		'month': str(now.month),
+		'year': str(now.year)
+	}
 
+def print_date():
+    date = get_date()
+    h_black_image = Image.new('1', (240, 240), 255)
+    draw_black = ImageDraw.Draw(h_black_image)
+    w=20
+    draw_black.text((w + 7, 5),
+					date['month'] + ' ' + date['day'] + date['day'] + ' ' + date['year'],
+					font=medium_font,
+					fill=0)
+    return h_black_image
+
+
+# Example usage:
 if __name__ == "__main__":
-    main()
+    frame = FrameWithGrid()
+
+    # Create an image with the current time
+    time_image = print_date()
+
+    # Allocate the time image to window 1
+    frame.allocate_window(time_image, window_number=1)
+
+    image2 = Image.open("dbws-robot.bmp")
+    image3 = Image.open("dbws-robot.bmp")
+
+    # Allocate images to specific windows
+    frame.allocate_window(image2, window_number=2)
+    frame.allocate_window(image3, window_number=3)
+
+    # Save the frame
+    frame.save_frame("output_frame.png")
