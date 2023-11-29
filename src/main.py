95ebdeb9c3225abc6314385c1f9af77ecb300721
Tilman Seeßelberg <tilman.seesselberg@hm.edu>
2023-11-29 14:35:19 +0100

    when error occures, screen will be cleared

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import frame_gen
 from lib import epd7in5_V2
 epd = epd7in5_V2.EPD()
-epd.init()
 
 def main():
-    img = frame_gen.create_image()
-    epd.display(epd.getbuffer(img))
-    epd.sleep()
+    try :
+        img = frame_gen.create_image()
+        epd.init()
+        epd.display(epd.getbuffer(img))
+    except:
+        epd.init()
+        epd.Clear()
+    finally:
+        epd.sleep()
 
 if __name__ == '__main__':
     main()
